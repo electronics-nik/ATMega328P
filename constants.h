0410719bd114b8781da34d1af2e00b992d352149
Nikolay <kiseev.nikolay@gmail.com>
2017-10-16 00:57:15 +0300

    First commit. Add constants.h file

---
+++ constants.h
@@ -0,0 +1,24 @@
+/*
+ * constants.h
+ * CPU: ATMega328P
+ * F_CPU: 16MHz
+ *
+ * Created: 15.10.2017 13:46:54
+ *  Author: Nikolay
+ */
+
+#define F_CPU		16000000UL
+#define F_SCL		100000UL
+#define BAUD_RATE	9600UL
+
+#include <avr/io.h>
+#include <util/twi.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <avr/pgmspace.h>
+#include <time.h>
+#include <stdbool.h>
+#include <avr/interrupt.h>
+
+
+
